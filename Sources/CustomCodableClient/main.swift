ec7bd8701d898ec3b11cbcf7c7b0a1dc1ca22d5e
Jay Hsia <jayhsia@m800.com>
2024-11-07 18:47:11 +0800

    Add Print

--- Sources/CustomCodableClient/main.swift
+++ Sources/CustomCodableClient/main.swift
@@ -1,4 +1,5 @@
 import CustomCodable
+import Foundation
 
 let a = 17
 let b = 25
@@ -12,4 +13,19 @@ struct CustomCodableString: Codable {
     @CodableKey(name: "OtherName")
     var propertyWithOtherName: String
     var propertyWithSameName: Bool
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        print("CodingKeys: \(CodingKeys.propertyWithOtherName)")
+        self.propertyWithOtherName = try container.decode(String.self, forKey: .propertyWithOtherName)
+        self.propertyWithSameName = try container.decode(Bool.self, forKey: .propertyWithSameName)
+    }
 }
+
+let json: [String: Any] = [
+    "OtherName": "1",
+    "propertyWithSameName": true
+]
+let jsonData = try JSONSerialization.data(withJSONObject: json, options: [])
+let data = try JSONDecoder().decode(CustomCodableString.self, from: jsonData)
+print(data)
